c436bb2196d6dd20c053f10b5f48bc1d623fe350
elhaykar <elhaykar@students.zhaw.ch>
2025-02-02 23:48:51 +0100

    formating

--- coin-change.c
+++ coin-change.c
@@ -12,16 +12,16 @@ coinChange* answerStruct = NULL;
 uint64_t** tabuArray = NULL;
 
 void printarr(int *arr, int size) {
-    if (size == 0) {
+    if(size == 0) {
         printf("[]\n");
     }
 
-    for (int i = 0; i < size; i++) {
-        if (size == 1) {
+    for(int i = 0; i < size; i++) {
+        if(size == 1) {
             printf("[%i]\n", arr[i]);
-        } else if (i == 0) {
+        } else if(i == 0) {
             printf("[%i, ", arr[i]);
-        } else if (i == size - 1) {
+        } else if(i == size - 1) {
             printf("%i]\n", arr[i]);
         } else {
             printf("%i, ", arr[i]);
@@ -30,17 +30,17 @@ void printarr(int *arr, int size) {
 }
 
 void printOneComb(int *arr, int size) {
-    if (size == 0) {
+    if(size == 0) {
         printf("[]\n");
         return;
     }
 
-    for (int i = 0; i < size; i++) {
-        if (size == 1) {
+    for(int i = 0; i < size; i++) {
+        if(size == 1) {
             printf("[%i]\n", arr[i]);
-        } else if (i == 0) {
+        } else if(i == 0) {
             printf("[%i", arr[i]);
-        } else if (i == size - 1 || arr[i] == 0) {
+        } else if(i == size - 1 || arr[i] == 0) {
             printf("]\n");
             return;
         } else {
@@ -51,16 +51,16 @@ void printOneComb(int *arr, int size) {
 
 void allocateTabuArray(int rows, int cols) {
     tabuArray = (uint64_t**)calloc(rows, sizeof(uint64_t*));
-    for (uint64_t i = 0; i < rows; i++) {
+    for(uint64_t i = 0; i < rows; i++) {
         tabuArray[i] = (uint64_t*)calloc(cols, sizeof(uint64_t));
     }
 }
 
 int changeRecursive(int amount, int* coins, int coinsSize) {
     // base case
-    if ((amount == *coins && amount == *(coins + coinsSize - 1)) || amount == 0) {
+    if((amount == *coins && amount == *(coins + coinsSize - 1)) || amount == 0) {
         return 1;
-    } else if (amount < 0 || coinsSize < 1) {
+    } else if(amount < 0 || coinsSize < 1) {
         return 0;
     }
 
@@ -77,13 +77,13 @@ int change(int amount, int* coins, int coinsSize) {
     const uint64_t rows = coinsSize + 1;
     allocateTabuArray(rows, cols);
 
-    for (uint64_t i = 0; i <= amount; ++i) {
-        for (uint64_t j = 0; j <= coinsSize; ++j) {
-            if (i == 0) {
+    for(uint64_t i = 0; i <= amount; ++i) {
+        for(uint64_t j = 0; j <= coinsSize; ++j) {
+            if(i == 0) {
                 *(*(tabuArray + j) + i) = 1;
                 continue;
             }
-            if (j == 0) {
+            if(j == 0) {
                 continue;
             }
 
@@ -92,7 +92,7 @@ int change(int amount, int* coins, int coinsSize) {
 
             *(*(tabuArray + j) + i) = *(*(tabuArray + j - 1) + i);
 
-            if (denomDif >= 0) {
+            if(denomDif >= 0) {
                 *(*(tabuArray + j) + i) += *(*(tabuArray + j) + denomDif);
             }
         }
@@ -107,21 +107,22 @@ coinChange* fillCombArrays(int numberCombinations, int amount, int* coins, int c
     int rows = numberCombinations;
     int cols = amount;
     int** combArray = (int**)calloc(rows, sizeof(int*));
-    for (int i = 0; i < rows; i++) {
+    for(int i = 0; i < rows; i++) {
         combArray[i] = (int*)calloc(cols, sizeof(int));
     }
 
-    const uint64_t amountColumn = amount; 
-    int i_ChangeGroup = 0;
-    uint64_t i_row = coinsSize;
+    uint64_t amountColumn = amount;
+    int changeGroupPos = 0;
     int changeGroup = 0;
-    int changeGroupPos = 0; 
+    
+    int i_ChangeGroup = 0;
+    uint64_t i_row = coinsSize; 
     while(i_ChangeGroup < numberCombinations) {
         uint64_t currentChangeGroupCount = *(*(tabuArray + i_row) + amountColumn);
         uint64_t belowChangeGroupCount = i_row - 1 > 0 ? *(*(tabuArray + i_row - 1) + amountColumn) : 0;
         uint64_t numberGroupWithCurrentDenomMax = currentChangeGroupCount - belowChangeGroupCount;
 
-        for (int i = 0; i < numberGroupWithCurrentDenomMax; ++i) {
+        for(int i = 0; i < numberGroupWithCurrentDenomMax; ++i) {
             *(*(combArray + changeGroup + i) + changeGroupPos) = *(coins + i_row - 1);
         }
 
@@ -138,12 +139,12 @@ coinChange* fillCombArrays(int numberCombinations, int amount, int* coins, int c
 }
 
 void printAllComb (int amount) {
-    if (answerStruct->numberCombinations == 0) {
+    if(answerStruct->numberCombinations == 0) {
         printf("[]\n\n");
         return;
     }
 
-    for (int i = 0; i < answerStruct->numberCombinations; ++i) {
+    for(int i = 0; i < answerStruct->numberCombinations; ++i) {
         printOneComb(*(answerStruct->combinations + i), amount);
     }
     printf("\n\n");
